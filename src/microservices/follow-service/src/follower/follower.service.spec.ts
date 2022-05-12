c99c68b7907efbc587c705b678714aee84232c50
MrSandyC <sandercoret@gmail.com>
2022-05-12 10:58:59 +0200

    fix: fixed tests

--- src/microservices/follow-service/src/follower/follower.service.spec.ts
+++ src/microservices/follow-service/src/follower/follower.service.spec.ts
@@ -1,4 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
+import { Follower } from './entities/follower.entity';
 import { FollowerService } from './follower.service';
 
 describe('FollowerService', () => {
@@ -6,7 +9,17 @@ describe('FollowerService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [FollowerService],
+      providers: [
+        FollowerService,
+        {
+          provide: getRepositoryToken(Follower),
+          useValue: {},
+        },
+        {
+          provide: getRepositoryToken(User),
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<FollowerService>(FollowerService);
