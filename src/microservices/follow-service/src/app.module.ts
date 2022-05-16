00e6099775f1ed8ffcaa43944f29118efeac8f10
MrSandyC <sandercoret@gmail.com>
2022-05-16 18:32:04 +0200

    feat: fixed issues with deployment on k8s

--- src/microservices/follow-service/src/app.module.ts
+++ src/microservices/follow-service/src/app.module.ts
@@ -6,14 +6,15 @@ import { FollowerModule } from './follower/follower.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user/entities/user.entity';
 
+// TODO: env file
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.MYSQL_HOST,
+      host: '10.42.1.6',
       port: 3306,
       username: 'root',
-      password: 'root',
+      password: 'password',
       database: 'quitter-follow',
       entities: [User],
       synchronize: true,
