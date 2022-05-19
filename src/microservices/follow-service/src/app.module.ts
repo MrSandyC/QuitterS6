fb5901aaee3f8f5a8e6bd53ffe2e6f63b668ea34
MrSandyC <sandercoret@gmail.com>
2022-05-19 12:10:30 +0200

    fix: using env in services now

--- src/microservices/follow-service/src/app.module.ts
+++ src/microservices/follow-service/src/app.module.ts
@@ -5,16 +5,18 @@ import { UserModule } from './user/user.module';
 import { FollowerModule } from './follower/follower.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user/entities/user.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.MYSQL_HOST,
+      host: String(process.env.MYSQL_HOST),
       port: parseInt(process.env.MYSQL_PORT),
-      username: process.env.mysql_username,
-      password: process.env.mysql_password,
-      database: process.env.database,
+      username: String(process.env.mysql_username),
+      password: String(process.env.mysql_password),
+      database: String(process.env.database),
       entities: [User],
       synchronize: true,
     }),
--- src/microservices/user-service/src/app.module.ts
+++ src/microservices/user-service/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,13 +8,14 @@ import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.MYSQL_HOST,
+      host: String(process.env.MYSQL_HOST),
       port: parseInt(process.env.MYSQL_PORT),
-      username: process.env.mysql_username,
-      password: process.env.mysql_password,
-      database: process.env.database,
+      username: String(process.env.mysql_username),
+      password: String(process.env.mysql_password),
+      database: String(process.env.database),
       entities: [User],
       synchronize: true,
     }),
