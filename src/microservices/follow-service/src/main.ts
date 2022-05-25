803f99a3fbf4e33be95684f442b9adb7bd56e126
MrSandyC <sandercoret@gmail.com>
2022-05-25 11:29:00 +0200

    feat: added RMQ to follow-service

--- src/microservices/follow-service/src/main.ts
+++ src/microservices/follow-service/src/main.ts
@@ -6,6 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.createMicroservice(AppModule, {
     transport: Transport.TCP,
     options: {
+      urls: [
+        `amqp://${process.env.RABBITMQ_DEFAULT_USER}:${process.env.RABBITMQ_DEFAULT_PASS}@${process.env.RABBITMQ_HOST}`,
+      ],
+      queue: String(process.env.RABBITMQ_FOLLOW_QUEUE),
       host: '0.0.0.0',
       port: 5003,
     },
