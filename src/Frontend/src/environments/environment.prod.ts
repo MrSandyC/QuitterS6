5e443fa370b4f4f27db8666243b9c0f05a4b24ca
MrSandyC <sandercoret@gmail.com>
2022-05-25 17:51:30 +0200

    fix: env for prod

--- src/Frontend/src/environments/environment.prod.ts
+++ src/Frontend/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
   apiURL: 'http://localhost:9080',
+  AUTH0_DOMAIN: 'quitter-dev.eu.auth0.com',
+  AUTH0_CLIENTID: 'FhAz272pqwzNvODJXWH2uXb6yVCWvPQ9',
 };
