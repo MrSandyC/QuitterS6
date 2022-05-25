754e190ba08370829efe034ae8825de080f316e3
MrSandyC <sandercoret@gmail.com>
2022-05-25 19:09:30 +0200

    fix: rabbitMQ linking for queet-service in gateway

--- src/gateway/src/queet/queet.module.ts
+++ src/gateway/src/queet/queet.module.ts
@@ -11,12 +11,7 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
         transport: Transport.RMQ,
         options: {
           urls: [
-            `amqp://${process.env.RABBITMQ_DEFAULT_USER}:` +
-              process.env.RABBITMQ_DEFAULT_PASS +
-              '@' +
-              process.env.RABBITMQ_HOST +
-              ':' +
-              process.env.RABBITMQ_PORT,
+            `amqp://${process.env.RABBITMQ_DEFAULT_USER}:${process.env.RABBITMQ_DEFAULT_PASS}@${process.env.RABBITMQ_HOST}`,
           ],
           queue: String(process.env.RABBITMQ_QUEET_QUEUE),
           queueOptions: {
