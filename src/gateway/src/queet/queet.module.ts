10e7d720f4f176a0580c5bb1e23e0ff94592da2b
MrSandyC <sandercoret@gmail.com>
2022-05-25 16:07:31 +0200

    feat: rabbitmq in kubernetes

--- src/gateway/src/queet/queet.module.ts
+++ src/gateway/src/queet/queet.module.ts
@@ -11,7 +11,12 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
         transport: Transport.RMQ,
         options: {
           urls: [
-            `amqp://${process.env.RABBITMQ_DEFAULT_USER}:${process.env.RABBITMQ_DEFAULT_PASS}@${process.env.RABBITMQ_HOST}`,
+            `amqp://${process.env.RABBITMQ_DEFAULT_USER}:` +
+              process.env.RABBITMQ_DEFAULT_PASS +
+              '@' +
+              process.env.RABBITMQ_HOST +
+              ':' +
+              process.env.RABBITMQ_PORT,
           ],
           queue: String(process.env.RABBITMQ_QUEET_QUEUE),
           queueOptions: {
--- src/microservices/queet-service/src/main.ts
+++ src/microservices/queet-service/src/main.ts
@@ -7,7 +7,10 @@ async function bootstrap() {
     transport: Transport.RMQ,
     options: {
       urls: [
-        `amqp://${process.env.RABBITMQ_DEFAULT_USER}:${process.env.RABBITMQ_DEFAULT_PASS}@${process.env.RABBITMQ_HOST}`,
+        `amqp://${process.env.RABBITMQ_DEFAULT_USER}:` +
+          process.env.RABBITMQ_DEFAULT_PASS +
+          '@' +
+          process.env.RABBITMQ_HOST,
       ],
       queue: String(process.env.RABBITMQ_QUEET_QUEUE),
       host: '0.0.0.0',
