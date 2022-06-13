3a2eef5d22c973ecc1e3299e86e72c44e603adb6
MrSandyC <sandercoret@gmail.com>
2022-06-13 09:49:43 +0200

    fix: fixed API url for deployment

--- src/Frontend/src/environments/environment.prod.ts
+++ src/Frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURL: 'http://localhost:9080',
+  apiURL: 'http://20.105.24.84',
   AUTH0_DOMAIN: 'quitter-dev.eu.auth0.com',
   AUTH0_CLIENTID: 'FhAz272pqwzNvODJXWH2uXb6yVCWvPQ9',
 };
